2b29bd3f2c813e6665fd1ea90ff383b9a283e9c2
Sean Matheis <97707408+seanmatheis@users.noreply.github.com>
2024-04-13 22:25:39 -0400

    Final Presentation

--- arduinoServer/websocketSingleModuleHotspot/websocketSingleModuleHotspot.ino
+++ arduinoServer/websocketSingleModuleHotspot/websocketSingleModuleHotspot.ino
@@ -16,14 +16,18 @@ String dataServer;
 //data from mega
 String dataMega;
 String bitstream;
+String dataIn;
 char currPlayer;
 char winner;
 char LinActPos;
+char restart;
 String sensorValue;
+String sensorValueFromMega;
 int sensorValueInt;
 char physicalReady;
 char remoteReady;
 int readyButton = 1;
+int bothReady;
 
 
 void setup() {
@@ -41,14 +45,17 @@ void setup() {
 
   Serial.print("Setup Complete");
 
+  //Initialize Data Streams
+  dataMega = "<00000000>";
+  dataServer = "<02800000>";
+
   //Initialize ready status
-  physicalReady = '1';
-  remoteReady = '1';
+  physicalReady = '0';
+  remoteReady = '0';
+  bothReady = 0;
 
   //Initialize currPlayer to 2;
   currPlayer = '2';
-
-
 
 }
 
@@ -62,46 +69,78 @@ void loop() {
   }
 
 
+  //Wait unit both players press ready button
+  while(bothReady == 0)
+  {
+
+    
+    if(physicalReady != '1'){
+      dataIn = Serial2.readString();
+      if(dataIn[5] == '1'){
+        physicalReady = '1';
+      }
+
+      if(physicalReady == '1'){
+        createPhysicalReady();
+        socket.send(dataMega);
+      }
+    }
+
+
+    if(physicalReady == '1' && remoteReady == '1'){
+      Serial2.println(dataServer);
+      bothReady = 1;
+      break;
+      break;
+    }
+
+    socket.poll();
+    
+  }
+
+
 
   //Current player - physical player, read from mega
   if(currPlayer == '0'){
 
     //Simulate waiting for linear actuator
     //Send data to mega
-    //wait for response from mega
-    delay(5000);
+    Serial.println(dataServer);
+    Serial2.println(dataServer);
 
+    sensorValue = "8";
 
-    //Get data from sensor currently random
-    sensorValueInt = random(0, 7);
-    sensorValue = String(sensorValueInt);
+    while(sensorValue == "8"){
+      dataIn = Serial2.readString();
+      sensorValueFromMega = dataIn[4];
+      //Serial.println("Sensor Value From Mega: ");
+      //Serial.println(sensorValueFromMega);
+      if(sensorValueFromMega == "0" || sensorValueFromMega == "1" || sensorValueFromMega == "2" || sensorValueFromMega == "3" || sensorValueFromMega == "4" || sensorValueFromMega == "5" ||sensorValueFromMega == "6"){
+          sensorValue = sensorValueFromMega;
+      }
 
-    currPlayer = '1';
-    changeCurrPlayer();
-    
+      Serial.print("o");
+      if(sensorValue != "8"){
+        break;
+        break;
+      }
+      delay(100);
+    }
+
+    //Get data from mega
+    Serial.println("Data Coming from Mega: ");
+    Serial.println(dataIn);
+
+    //Send to socket
     //Create data packet
     createDataPacket();
-    Serial.println(dataMega);
+    currPlayer = '1';
+    changeCurrPlayer();
+    //Serial.println(dataMega);
     socket.send(dataMega);
 
-    delay(1000);
+    delay(100);
 
-    
-
-    
-
-
-
-
-    //dataMega = Serial2.readString();
-    //Test with random number
-    //Get data from mega
-    //dataMega = "<0006000>";
-    //sensorValue = dataMega[4];
-
-    //Send sensor data to websocket
-    //dataServer[4] = sensorValue;
-    //socket.send(dataServer);
   }
   //Current Player - remote player read from server n
   else if(currPlayer == '1'){
@@ -113,11 +152,11 @@ void loop() {
   else{
   }
 
-/*
-  while(currPlayer == '1'){
-    Serial.println("loop");
-    socket.poll();
-  }*/
+  if(winner == '0' || winner == '1'){
+    bothReady = 0;
+    physicalReady = '0';
+    remoteReady = '0';
+  }
 
   socket.poll();
 
@@ -128,11 +167,9 @@ void loop() {
 
 void handleMessage(WebsocketsMessage message){
   Serial.println(message.data());
-  Serial2.println(message.data());
+  //Serial2.println(message.data());
   dataServer = message.data();
   parseData(dataServer);
-
-
 }
 
 void handleEvent(WebsocketsEvent event, WSInterfaceString data){
@@ -172,13 +209,39 @@ void parseData(String dataServer){
   currPlayer = dataServer[1];
   winner = dataServer[2];
   LinActPos = dataServer[3];
+  restart = dataServer[7];
 
+  if(restart == '1'){
+    bothReady = 0;
+    physicalReady = '0';
+    remoteReady = '0';
+  }
+
+  if(dataServer[6] == '1'){
+    remoteReady = '1';
+  }
+  else{
+    remoteReady = '0';
+  }
+
+}
+
+void receiveFromMega(String dataMega){
+  sensorValue = dataMega[4];
+  physicalReady = dataMega[5];
+}
+
+void createPhysicalReady(){
+
+  dataMega = dataServer;
+  dataMega[5] = physicalReady;
 }
 
 void createDataPacket(){
   dataMega = dataServer;
   dataMega[4] = sensorValue[0];
-  Serial.println(dataServer);
+  dataMega[1] = currPlayer;
+  Serial.println("Data Packet being sent: ");
   Serial.println(dataMega);
 }
 
